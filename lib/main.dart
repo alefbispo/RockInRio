d98a32bf0d064e862f6a28c5248caa3cd04b6882
alefbispo <Blacksabbath21@>
2022-04-15 20:44:02 -0300

    criando Stateful e a pagina home com metodo state

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,29 @@ void main() {
 }
 
 class RockInRio extends StatelessWidget {
+  const RockInRio({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Rock in Rio',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
