9245c50fc3089d29fc8f1fa407a2ed56334d2053
alefbispo <Blacksabbath21@>
2022-04-15 20:28:40 -0300

    estruturando a pagina

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,12 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(RockInRio());
+}
+
+class RockInRio extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
